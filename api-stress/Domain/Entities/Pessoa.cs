b8f4e3bd68a24f96d2f8e211ca285202a19ba09d
Murilo Fontana <fontana.murilo@gmail.com>
2023-08-16 21:03:43 -0300

    feat add pessoa entitiy

---
+++ api-stress/Domain/Entities/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+  public class Pessoa
+  {
+    public Guid Id { get; set; }
+    public string Apelido { get; set; }
+    public string Nome { get; set; }
+    public DateOnly Nascimento { get; set; }
+    public IEnumerable<string> Stack { get; set; } = Enumerable.Empty<string>();
+  }
+}
