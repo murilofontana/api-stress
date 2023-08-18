9e06638063d6af8f0e6e9d506142a8fd6ef4bc03
Murilo Fontana <fontana.murilo@gmail.com>
2023-08-17 22:24:36 -0300

    feat depency injection

---
+++ api-stress/api-stress/Extensions/RepositoryExtension.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces.Repositories;
+using Infra.Repositories;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace api_stress.Extensions
+{
+  public static class RepositoryExtension
+  {
+    public static void AddRepositories(
+           this IServiceCollection services)
+    {
+      services.AddTransient<IPessoaRepository, PessoaRepository>();
+    }
+  }
+}
--- api-stress/api-stress/Program.cs
+++ api-stress/api-stress/Program.cs
@@ -1,9 +1,15 @@
+using api_stress.Extensions;
+using Infra.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<DataContext>();
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
