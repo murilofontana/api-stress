d6e33747adeef0e320a0a1e870c8e9f2fb127300
Murilo Fontana <fontana.murilo@gmail.com>
2023-08-20 18:29:46 -0300

    fix removing default endpoints template

--- api-stress/api-stress/Program.cs
+++ api-stress/api-stress/Program.cs
@@ -36,24 +36,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast = Enumerable.Range(1, 5).Select(index =>
-     new WeatherForecast
-     (
-         DateTime.Now.AddDays(index),
-         Random.Shared.Next(-20, 55),
-         summaries[Random.Shared.Next(summaries.Length)]
-     ))
-      .ToArray();
-  return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.MapGet("/pessoas/{id:guid}", async ([FromRoute(Name = "id")] Guid ? id, [FromServices]IPessoaRepository _pessoaRepository) => 
 {
@@ -68,6 +50,7 @@ app.MapPost("/pessoas", async ([FromBody]PessoaRequest requestPessoa, [FromServi
   try
   {
     var pessoa = new Pessoa(requestPessoa.Apelido, requestPessoa.Nome, requestPessoa.Nascimento, requestPessoa.Stack);
+    if (!pessoa.IsValid()) return Results.UnprocessableEntity("UnprocessableEntity");
     await _pessoaRepository.Insert(pessoa);
 
     return Results.Created(new Uri($"/pessoas/{pessoa.Id}"), pessoa);
@@ -80,8 +63,3 @@ app.MapPost("/pessoas", async ([FromBody]PessoaRequest requestPessoa, [FromServi
 });
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
