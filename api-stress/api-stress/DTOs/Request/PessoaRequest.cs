1900abd90b3f44c8b9d31aa75b589c4cb514f6b7
Murilo Fontana <fontana.murilo@gmail.com>
2023-08-20 18:22:11 -0300

    feat adding request dto for pessoa

---
+++ api-stress/api-stress/DTOs/Request/PessoaRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace api_stress.DTOs.Request
+{
+  public class PessoaRequest
+  {
+    public string Apelido { get; set; } = string.Empty;
+    public string Nome { get; set; } = string.Empty;
+    public DateOnly Nascimento { get; set; }
+    public List<string> Stack { get; set; } = new List<string>();
+
+  }
+}
